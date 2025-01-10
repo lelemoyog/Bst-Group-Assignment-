e891175f15eafe094ced471972308c1a24dc643d
Ruthgitau <gitaur72@gmail.com>
2025-01-10 21:44:05 +0300

    Node in a Binary Search Tree

---
+++ BSTNode.java
@@ -0,0 +1,13 @@
+public class BSTNode {
+    public String key;
+    public String value;
+    public BSTNode left;
+    public BSTNode right;
+
+    public BSTNode(String key, String value) {
+        this.key = key;
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
